85474021a4a5a1bdb6b35a7650c0e2c4211eddff
Allovelle <nominee09_fresher@icloud.com>
2025-01-03 16:48:03 -0500

    Initial Commit

---
+++ src/resize-dock.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+echo ':cherries:'  # More Emojis: https://gist.github.com/rxaviers/7360908
+echo '---'
+echo "Toggle Dock | shell='$(which macos-resize-dock)'"
